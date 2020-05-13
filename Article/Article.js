421a5c4422889876313a9e9d9fcffedd986fb5d4
Jeff Clark <jeffclarktopeka@gmail.com>
2020-05-12 22:51:54 -0500

    began building the articleMaker component

--- Article/Article.js
+++ Article/Article.js
@@ -111,3 +111,31 @@ const data = [
 
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 */
+
+const articleMaker = (title, date, firstParagraph, secondParagraph, thirdParagraph) => {
+
+  const article = document.createElement('div')
+  const header = document.createElement('h2')
+  const articleDate = document.createElement('p')
+  const firstP = document.createElement('p')
+  const secondP = document.createElement('p')
+  const thirdP = document.createElement('p')
+  const expandBtn = document.createElement('span')
+
+  article.append(header, articleDate, firstP, secondP, thirdP, expandBtn)
+
+  article.classList.add('article')
+  articleDate.classList.add('date')
+  expandBtn.classList.add('expandButton')
+
+  header.textContent = title
+  articleDate.textContent = date
+  firstP.textContent = firstParagraph
+  secondP.textContent = secondParagraph
+  thirdP.textContent = thirdParagraph
+
+  return article;
+
+} 
+
+const articleComponent = document
